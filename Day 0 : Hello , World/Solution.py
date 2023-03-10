5d8afc53d7bd4f4550f6e60305eb6384006a9125
D Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2023-03-11 01:12:09 +0530

    Create Solution.py

---
+++ Day 0 : Hello , World/Solution.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__' :  # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+  input_string = input()
+
+  # Print a string literal saying "Hello, World." to stdout.
+  print('Hello, World.')
+  print(input_string)
