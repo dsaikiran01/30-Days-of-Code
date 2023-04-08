319a864ee6095b3d33d9b1d8290d26838c95bb38
D Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2023-04-08 23:29:12 +0530

    Create Solution.cpp

---
+++ Day 19 : Interfaces/Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class AdvancedArithmetic
+{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+
+class Calculator : public AdvancedArithmetic
+{
+    public:
+        int divisorSum(int n)
+        {
+            int sumOfDivisors = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                    sumOfDivisors += i;
+            }
+            return sumOfDivisors;
+        }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
