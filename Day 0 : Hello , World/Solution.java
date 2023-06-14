b881576a5eecff705d5acbaccfd4c3094615726a
Dumpala Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2023-06-14 22:44:34 +0530

    Create Solution.java

---
+++ Day 0 : Hello , World/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution 
+{
+    public static void main(String[] args)
+  {
+        Scanner senObj = new Scanner(System.in);
+        String sentence = senObj.nextLine();
+        
+        System.out.println("Hello, World.\n" + sentence);
+    }
+}
