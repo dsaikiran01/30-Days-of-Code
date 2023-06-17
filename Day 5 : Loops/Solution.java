67803ebb39e1140142fcc8ac0e79fce5a981c2e7
Dumpala Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2023-06-17 13:46:05 +0530

    Create Solution.java

---
+++ Day 5 : Loops/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        
+        for(int i = 1; i < 11; i++)
+        {
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+        
+        scan.close();
+    }
+}
