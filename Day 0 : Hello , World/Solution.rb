79628dc6c0234ce2b31eb812baa89bb1aba5f212
Dumpala Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2024-05-05 18:47:56 +0530

    Create Solution.rb

---
+++ Day 0 : Hello , World/Solution.rb
@@ -0,0 +1,6 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = gets.chomp
+
+# Print "Hello, World." and input string to stdout.
+puts 'Hello, World.'
+puts input_string
