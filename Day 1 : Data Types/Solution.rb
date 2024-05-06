fe2efb8ee0719201b73858998ebb22d47decacd7
Dumpala Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2024-05-06 17:43:39 +0530

    Create Solution.rb

---
+++ Day 1 : Data Types/Solution.rb
@@ -0,0 +1,16 @@
+# Number: integer
+i = 4
+# Number: float
+d = 4.0
+# String
+s = 'HackerRank '
+
+j = gets.to_i
+e = gets.to_f
+t = gets.chomp
+
+# sum of integers and floating numbers
+puts i+j
+puts d+e
+# Concatenation of String variables
+puts(s+t)
