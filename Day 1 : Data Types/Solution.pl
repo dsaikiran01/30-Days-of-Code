d5cdc7688f7373fd791674eb1213f1773448b845
Dumpala Sai Kiran <108150261+dsaikiran01@users.noreply.github.com>
2024-08-10 21:08:02 +0530

    Update Day 1 : Data Types

---
+++ Day 1 : Data Types/Solution.pl
@@ -0,0 +1,27 @@
+$i = 4;
+$d = 4.0;
+$s = 'HackerRank ';
+
+#Declare second integer, double, and String variables.
+
+#Read and save an integer, double, and String to your variables.
+
+$j = <STDIN>;
+
+$e = <STDIN>;
+
+$t = <STDIN>;
+
+# Print the sum of both integer variables on a new line.
+
+print $i + $j. "\n";
+
+# Print the sum of the double variables on a new line.
+
+$f = sprintf("%.1f", $d + $e);
+
+print $f . "\n";
+
+#Concate nate and print the String variables on a new line The 's' variable above should be printed first.
+
+print $s . $t;
